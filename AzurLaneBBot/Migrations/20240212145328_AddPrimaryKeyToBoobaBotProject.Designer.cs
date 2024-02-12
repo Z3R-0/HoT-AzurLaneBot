eed47d0349dc2b9cbbba0461d2cffbcf3d94e179
Z3R-0 <timmeermans@live.nl>
2024-02-12 15:56:22 +0100

    Updating database model

--- AzurLaneBBot/Migrations/20240212145328_AddPrimaryKeyToBoobaBotProject.Designer.cs
+++ AzurLaneBBot/Migrations/20240212145328_AddPrimaryKeyToBoobaBotProject.Designer.cs
@@ -2,6 +2,7 @@
 using AzurLaneBBot.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AzurLaneBBot.Migrations
 {
     [DbContext(typeof(AzurlanedbContext))]
-    partial class AzurlanedbContextModelSnapshot : ModelSnapshot
+    [Migration("20240212145328_AddPrimaryKeyToBoobaBotProject")]
+    partial class AddPrimaryKeyToBoobaBotProject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.8");
