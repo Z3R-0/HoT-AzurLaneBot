2f04cf618df8f7e8c85cf484e1650d8f2791bcbb
Z3R-0 <timmeermans@live.nl>
2024-05-04 23:22:05 +0200

    Feature/update delete management commands (#13)

---
+++ AzurLaneBBot/Modules/Commands/ManagementCommandComponentHandlers.cs
@@ -0,0 +1,41 @@
+﻿using AzurLaneBBot.Database.DatabaseServices;
+using AzurLaneBBot.Database.Models;
+using Discord.Interactions;
+using Discord.WebSocket;
+using ReshDiscordNetLibrary;
+
+namespace AzurLaneBBot.Modules.Commands {
+    public class ManagementCommandComponentHandlers : BotInteraction<SocketMessageComponent> {
+        private IDatabaseService _dbService;
+
+        public ManagementCommandComponentHandlers(AzurlanedbContext dbService) {
+            _dbService = new AzurDbContextDatabaseService(dbService);
+        }
+
+        [ComponentInteraction(ManagementCommands.NextButtonCustomId + "*")]
+        public async Task HandleNextButton(int current) {
+            await DeferAsync();
+            current++;
+            
+            await Context.Interaction.Message.DeleteAsync();
+
+            var entries = _dbService.GetAllBBPShips();
+            var pagination = ManagementCommands.DisplayPage(entries, current, (entries.Count() + ManagementCommands.EntriesPerPage - 1) / ManagementCommands.EntriesPerPage, ManagementCommands.EntriesPerPage);
+
+            await FollowupAsync(embed: pagination.EmbedBuilder.Build(), components: pagination.ComponentBuilder.Build());
+        }
+
+        [ComponentInteraction(ManagementCommands.PreviousButtonCustomId + "*")]
+        public async Task HandlePreviousButton(int current) {
+            await DeferAsync();
+            current--;
+
+            await Context.Interaction.Message.DeleteAsync();
+
+            var entries = _dbService.GetAllBBPShips();
+            var pagination = ManagementCommands.DisplayPage(entries, current, (entries.Count() + ManagementCommands.EntriesPerPage - 1) / ManagementCommands.EntriesPerPage, ManagementCommands.EntriesPerPage);
+
+            await FollowupAsync(embed: pagination.EmbedBuilder.Build(), components: pagination.ComponentBuilder.Build());
+        }
+    }
+}
