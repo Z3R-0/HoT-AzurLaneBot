087b88513e5723a23d2aeb6bf454d8ead87cdab3
Z3R-0 <timmeermans@live.nl>
2024-05-06 21:28:15 +0200

    Initial set-up

---
+++ AzurLaneBBot/Modules/Commands/GameCommands.cs
@@ -0,0 +1,19 @@
+﻿using AzurLaneBBot.Database.DatabaseServices;
+using Discord.Interactions;
+using Discord.WebSocket;
+using ReshDiscordNetLibrary;
+
+namespace AzurLaneBBot.Modules.Commands {
+    public class GameCommands(IDatabaseService dbService) : BotInteraction<SocketSlashCommand> {
+        private readonly IDatabaseService _dbService = dbService;
+
+
+        [SlashCommand("guess-ship", "Guess a ship (or skin)")]
+        public async Task HandleGuessShipSlash(
+            [Choice("Yes", "Yes")]
+            [Choice("No", "No")]
+            string allowSkins) {
+
+        }
+    }
+}
