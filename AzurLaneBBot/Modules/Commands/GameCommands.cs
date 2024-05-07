10d453489a274ea42165def47bf90467669ccd76
Z3R-0 <timmeermans@live.nl>
2024-05-07 07:39:54 +0200

    Working on command

--- AzurLaneBBot/Modules/Commands/GameCommands.cs
+++ AzurLaneBBot/Modules/Commands/GameCommands.cs
@@ -7,13 +7,18 @@ namespace AzurLaneBBot.Modules.Commands {
     public class GameCommands(IDatabaseService dbService) : BotInteraction<SocketSlashCommand> {
         private readonly IDatabaseService _dbService = dbService;
 
-
         [SlashCommand("guess-ship", "Guess a ship (or skin)")]
         public async Task HandleGuessShipSlash(
             [Choice("Yes", "Yes")]
             [Choice("No", "No")]
             string allowSkins) {
+            var legalShipChoices = _dbService.GetAllBBPShips().Where(
+                ship => !string.IsNullOrEmpty(ship!.ImageUrl)
+                && (allowSkins == "No" && string.IsNullOrEmpty(ship.IsSkinOf)));
 
+            var conditionalText = allowSkins == "Yes" ? " or skin " : "";
+
+            var embedBuilder = DiscordUtilityMethods.GetEmbedBuilder($"What is the name of this ship{conditionalText}?");
         }
     }
 }
